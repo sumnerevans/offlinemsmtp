9baafd9a87bb801eca0651b0cc8e83649e6172e0
Sumner Evans <sumner.evans98@gmail.com>
2018-07-07 21:39:07 -0600

    Fix major regression

--- offlinemsmtp/daemon.py
+++ offlinemsmtp/daemon.py
@@ -1,7 +1,7 @@
 import os
 import time
 from queue import Queue
-from subprocess import PIPE, call
+from subprocess import PIPE, run
 
 from gi.repository import Notify
 from watchdog.events import FileSystemEventHandler
@@ -60,7 +60,7 @@ class Daemon(FileSystemEventHandler):
             command = [
                 '/usr/bin/msmtp', '-C', self.config_file, *msmtp_args.split()
             ]
-            sender = call(
+            sender = run(
                 command,
                 input=message_content,
                 stdout=PIPE,
