45d1c8fc4962ecdfc394c0e863a0fdf98522d5de
Sumner Evans <sumner.evans98@gmail.com>
2019-12-06 23:52:12 -0700

    Fixed Publish GitLab script

--- cicd/publish-gitlab-release.sh
+++ cicd/publish-gitlab-release.sh
@@ -39,8 +39,8 @@ data="
     \"assets\": {
         \"links\": [
             {
-                \"name\": \"tracktime-${CI_COMMIT_TAG:1}.tar.gz\",
-                \"url\": \"${CI_PROJECT_URL}/-/jobs/artifacts/${CI_COMMIT_TAG}/raw/dist/tracktime-${CI_COMMIT_TAG:1}.tar.gz?job=build\"
+                \"name\": \"offlinemsmtp-${CI_COMMIT_TAG:1}.tar.gz\",
+                \"url\": \"${CI_PROJECT_URL}/-/jobs/artifacts/${CI_COMMIT_TAG}/raw/dist/offlinemsmtp-${CI_COMMIT_TAG:1}.tar.gz?job=build\"
             }
         ]
     }
