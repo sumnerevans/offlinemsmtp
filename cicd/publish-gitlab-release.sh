65f81c6feffbef4bc2d17e55594c401178b2fddb
Sumner Evans <me@sumnerevans.com>
2020-06-02 01:28:21 -0600

    Fix publish release script for next time

--- cicd/publish-gitlab-release.sh
+++ cicd/publish-gitlab-release.sh
@@ -2,6 +2,13 @@
 
 set -e
 
+echo "VARIABLES"
+echo "CI_COMMIT_TAG=${CI_COMMIT_TAG}"
+echo "CI_API_V4_URL=${CI_API_V4_URL}"
+echo "CI_PROJECT_ID=${CI_PROJECT_ID}"
+echo "CI_PIPELINE_ID=${CI_PIPELINE_ID}"
+echo "CI_PROJECT_URL=${CI_PROJECT_URL}"
+
 # The release notes for this version should be the first line of the CHANGELOG.
 if [[ $(head -n 1 CHANGELOG.rst) == "${CI_COMMIT_TAG}" ]]; then
     # Extract all of the bullet points and other things until the next header.
@@ -14,6 +21,10 @@ if [[ $(head -n 1 CHANGELOG.rst) == "${CI_COMMIT_TAG}" ]]; then
                 break
             fi
             first=0
+        fi
+
+        if [[ "${l:0:11}" == ":Milestone:" ]]; then
+            milestone="$(echo $l | sed -n 's/:Milestone: \(.*\)/\1/p')"
         fi
     done < CHANGELOG.rst
 
@@ -30,12 +41,18 @@ fi
 
 description=$(echo "$description" | rst2html5 --no-indent --template "{body}" | sed -e 's/\"/\\\"/g')
 
+milestones=""
+if [[ "${milestone}" != "" ]]; then
+    milestones=",\"milestones\":[\"${milestone}\"]"
+fi
+
 url="${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/releases"
 data="
 {
     \"name\": \"${CI_COMMIT_TAG}\",
     \"tag_name\": \"${CI_COMMIT_TAG}\",
     \"description\": \"${description}\",
+    ${milestones}
     \"assets\": {
         \"links\": [
             {
@@ -57,4 +74,4 @@ curl \
     --header "PRIVATE-TOKEN: ${RELEASE_PUBLISH_TOKEN}" \
     --data "$data" \
     --request POST \
-    ${CI_API_V4_URL}/projects/${CI_PROJECT_ID}/releases
+    $url
