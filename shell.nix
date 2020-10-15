f202ce5810d3d59339597a70e973746b4e3d7c06
Sumner Evans <me@sumnerevans.com>
2020-10-15 10:38:08 -0600

    Use rnix-lsp for formatting shell.nix

--- shell.nix
+++ shell.nix
@@ -1,13 +1,15 @@
 { pkgs ? import <nixpkgs> {} }: with pkgs; let
   py = python38.override {
     packageOverrides = self: super: {
-      pycairo = super.pycairo.overridePythonAttrs (oldAttrs: rec {
-        version = "1.20.0";
-        src = oldAttrs.src.override {
-          inherit version;
-          sha256 = "5695a10cb7f9ae0d01f665b56602a845b0a8cb17e2123bfece10c2e58552468c";
-        };
-      });
+      pycairo = super.pycairo.overridePythonAttrs (
+        oldAttrs: rec {
+          version = "1.20.0";
+          src = oldAttrs.src.override {
+            inherit version;
+            sha256 = "5695a10cb7f9ae0d01f665b56602a845b0a8cb17e2123bfece10c2e58552468c";
+          };
+        }
+      );
     };
   };
 in
@@ -20,6 +22,7 @@ pkgs.mkShell {
 
   buildInputs = [
     libnotify
+    rnix-lsp
   ];
 
   propagatedBuildInputs = with python3Packages; [
@@ -28,12 +31,16 @@ pkgs.mkShell {
     pass
     pkg-config
     poetry
-    (python38.withPackages (ps: with ps; [
-      pygobject3
-      pycairo
-      watchdog
-      pkgconfig
-    ]))
+    (
+      python38.withPackages (
+        ps: with ps; [
+          pygobject3
+          pycairo
+          watchdog
+          pkgconfig
+        ]
+      )
+    )
   ];
 
   shellHook = ''
